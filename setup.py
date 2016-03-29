e5451e5064a302e1e1f2cfd0ca69188dd3d2633c
Petr Skramovsky <petr.skramovsky@gmail.com>
2016-03-29 10:26:01 +0200

    version increased

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cleaREST",
-    version="0.1.0",
+    version="0.2.0",
     author="Petr Skramovsky",
     author_email="petr.skramovsky@gmail.com",
     description="Light-weight Python framework for building REST APIs.",
